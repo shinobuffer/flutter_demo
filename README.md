6c457625db4bf82522912fb2aa79e182d149adc4
Bersder3000 <B1747322151@outlook.com>
2021-02-20 23:53:00 +0800

    init: flutter app initialization

---
+++ README.md
@@ -0,0 +1,15 @@
+## 可以考虑的插件
+icon查询-https://material.io/resources/icons/?style=baseline
+
+`Dio`-网络请求-https://github.com/flutterchina/dio/blob/master/README-ZH.md
+`bot_toast`-toast弹窗-https://github.com/MMMzq/bot_toast/blob/master/README_zh.md
+`flutter_xupdate`-安卓升级功能-https://segmentfault.com/a/1190000021720248
+`multi_image_picker`-相册选择，类似于微信-https://www.jianshu.com/p/3e9c988a812a
+`flutter_datetime_picker`-时间日期滚轮选择
+`uuid`-uuid生成
+`package_info`-获取应用包信息（版本等）
+`device_info`-获取设备信息
+`provider`-全局状态管理
+`flustars`-flutter常用工具库-https://github.com/Sky24n/flustars
+`flutter-swiper`-轮播图-https://github.com/best-flutter/flutter_swiper/blob/master/README-ZH.md
+`flutter-markdown`-markdown解析-
---
+++ lib/pages/Tabs.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeTabs extends StatefulWidget {
+  HomeTabs({Key key}) : super(key: key);
+
+  @override
+  _HomeTabsState createState() => _HomeTabsState();
+}
+
+class _HomeTabsState extends State<HomeTabs> {
+  final List<Map> tabItems = [
+    {
+      'icon': Icons.menu_book_rounded,
+      'label': '学习',
+    },
+    {
+      'icon': Icons.explore,
+      'label': '发现',
+    },
+    {
+      'icon': Icons.account_circle_rounded,
+      'label': '我的',
+    },
+  ];
+  int curIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: curIndex,
+      fixedColor: Color(0xff5abebc),
+      items: tabItems.map((e) => BottomNavigationBarItem(icon: Icon(e['icon']), label: e['label'])).toList(),
+      onTap: (index) {
+        setState(() {
+          curIndex = index;
+        });
+      },
+    );
+  }
+}
